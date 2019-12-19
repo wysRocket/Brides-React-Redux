2d0c7e61cc9c121f0f086fa2ca2491d789f3d6b0
wysMyFree <vkvirenko@icloud.com>
2019-12-19 13:01:10 +0200

    Updates

---
+++ precache-manifest.1f010ce9ee34f73cdbb783204c54c66e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe998125bdaeae8fa10db9bab25d3bfc",
+    "url": "/myReactApp/index.html"
+  },
+  {
+    "revision": "0b4a9fa8f58dc0c60cab",
+    "url": "/myReactApp/static/css/3.4d7dbf98.chunk.css"
+  },
+  {
+    "revision": "d18543bb27c7b45f048b",
+    "url": "/myReactApp/static/css/4.8a8b4973.chunk.css"
+  },
+  {
+    "revision": "b528f46a09d76f3e0f6b",
+    "url": "/myReactApp/static/css/main.2656998b.chunk.css"
+  },
+  {
+    "revision": "216fa638e99dfb129393",
+    "url": "/myReactApp/static/js/2.93ed349a.chunk.js"
+  },
+  {
+    "revision": "0b4a9fa8f58dc0c60cab",
+    "url": "/myReactApp/static/js/3.498320f9.chunk.js"
+  },
+  {
+    "revision": "d18543bb27c7b45f048b",
+    "url": "/myReactApp/static/js/4.3ea51f6d.chunk.js"
+  },
+  {
+    "revision": "b528f46a09d76f3e0f6b",
+    "url": "/myReactApp/static/js/main.2d166c64.chunk.js"
+  },
+  {
+    "revision": "6d18f3826caa64c7c580",
+    "url": "/myReactApp/static/js/runtime-main.64ace40a.js"
+  },
+  {
+    "revision": "dd23b76bda85dbaf1bded935bb426af8",
+    "url": "/myReactApp/static/media/chat-girl-foto.dd23b76b.jpg"
+  },
+  {
+    "revision": "358035aa9057bdb516ccf7023d2e3ee7",
+    "url": "/myReactApp/static/media/danny.358035aa.jpg"
+  },
+  {
+    "revision": "11a70194f0bca151fe29fa5d5307cf4d",
+    "url": "/myReactApp/static/media/danny2.11a70194.jpg"
+  },
+  {
+    "revision": "6fc2c0f992e7528dcbe9a37245980f21",
+    "url": "/myReactApp/static/media/danny3.6fc2c0f9.jpg"
+  },
+  {
+    "revision": "f8c3ef2e4c72020b1c7d644ea55c64c9",
+    "url": "/myReactApp/static/media/girl-list-mini.f8c3ef2e.jpg"
+  },
+  {
+    "revision": "7d98bd1d1d34cc8539241d97d7e01439",
+    "url": "/myReactApp/static/media/photo.7d98bd1d.jpg"
+  },
+  {
+    "revision": "be45ecdc3aa6138a803c710a2dedb1ce",
+    "url": "/myReactApp/static/media/photo2.be45ecdc.jpg"
+  },
+  {
+    "revision": "510c4275b8322c8b7cfd60629868ecde",
+    "url": "/myReactApp/static/media/photo3.510c4275.jpg"
+  }
+]);
